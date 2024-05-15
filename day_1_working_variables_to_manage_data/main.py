5b04f3bbc503ba67a3e57177c95d8deb349f968a
juansechaux <juansechaux@hotmail.com>
2024-05-14 21:01:30 -0500

    Band name generator

---
+++ day_1_working_variables_to_manage_data/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.\nWhat is the name of the city you grew up in?")
+city = input()
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
